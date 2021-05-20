1b3dffbe48c195644566d0394b81eda8537f0a90
Helmi Arghya Santosa <49424708+herghys@users.noreply.github.com>
2021-05-20 21:01:23 +0700

    Scriptable Object and Custom Editor for Periodic Elements

---
+++ Assets/Editor/ElementDataEditor.cs
@@ -0,0 +1,73 @@
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Element))]
+public class ElementDataEditor : Editor
+{
+    #region Variables
+    SerializedProperty elementNumProp = null;
+    SerializedProperty elementNameProp = null;
+    SerializedProperty atomicMassProp = null;
+    SerializedProperty elementTypeProp = null;
+    SerializedProperty electronConfigprop = null;
+    SerializedProperty oxydationStateProp = null;
+    SerializedProperty electronegativityProp = null;
+    SerializedProperty atomicRadProp = null;
+    SerializedProperty ionizationEnergyProp = null;
+    SerializedProperty electronAffinityProp = null;
+    SerializedProperty meltingPointProp = null;
+    SerializedProperty boilingPointProp = null;
+    SerializedProperty densityProp = null;
+    SerializedProperty yearDiscoveredProp = null;
+
+    #endregion
+
+    #region Unity Defaults
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        #region Fetch Props
+        elementNumProp          = serializedObject.FindProperty("_elementNum");
+        elementNameProp         = serializedObject.FindProperty("_elementName");
+        elementTypeProp         = serializedObject.FindProperty("_elementType");
+        atomicMassProp          = serializedObject.FindProperty("_atomicMass");
+        electronConfigprop      = serializedObject.FindProperty("_electronConfig");
+        oxydationStateProp      = serializedObject.FindProperty("_oxydationState");
+        electronegativityProp   = serializedObject.FindProperty("_electronegativity");
+        atomicRadProp           = serializedObject.FindProperty("_atomicRadius");
+        ionizationEnergyProp    = serializedObject.FindProperty("_ionizationEnergy");
+        electronAffinityProp    = serializedObject.FindProperty("_electronAffinity");
+        meltingPointProp        = serializedObject.FindProperty("_meltingPoint");
+        boilingPointProp        = serializedObject.FindProperty("_boilingPoint");
+        densityProp             = serializedObject.FindProperty("_density");
+        yearDiscoveredProp      = serializedObject.FindProperty("_yearDiscovered");
+        #endregion
+    }
+
+    // Update is called once per frame
+    public override void OnInspectorGUI()
+    {
+        #region Style
+        GUIStyle textAreaStyle = new GUIStyle(EditorStyles.textArea)
+        {
+            fontSize = 12, fixedHeight = 15, alignment = TextAnchor.MiddleLeft
+        };
+        #endregion
+        GUILayout.Label("Elements", EditorStyles.miniLabel);
+        elementNumProp.intValue = EditorGUILayout.IntField("Element Number", elementNumProp.intValue, textAreaStyle); GUILayout.FlexibleSpace();
+        elementNameProp.stringValue = EditorGUILayout.TextField("Element Name", elementNameProp.stringValue, textAreaStyle); GUILayout.FlexibleSpace();
+        EditorGUILayout.PropertyField(elementTypeProp, new GUIContent("Element Type", "Specify this element type")); GUILayout.FlexibleSpace();
+        atomicMassProp.doubleValue = EditorGUILayout.DoubleField("Atomic Mass", atomicMassProp.doubleValue, textAreaStyle); GUILayout.FlexibleSpace();
+        electronConfigprop.stringValue = EditorGUILayout.TextField("Electron Config", electronConfigprop.stringValue, textAreaStyle); GUILayout.FlexibleSpace();
+        oxydationStateProp.stringValue = EditorGUILayout.TextField("Oxydation State", oxydationStateProp.stringValue, textAreaStyle); GUILayout.FlexibleSpace();
+        electronegativityProp.doubleValue = EditorGUILayout.DoubleField("Electronegativity", electronegativityProp.doubleValue, textAreaStyle); GUILayout.FlexibleSpace();
+        atomicRadProp.doubleValue = EditorGUILayout.DoubleField("Atomic Radius", atomicRadProp.doubleValue, textAreaStyle); GUILayout.FlexibleSpace();
+        ionizationEnergyProp.doubleValue = EditorGUILayout.DoubleField("Ionization Energy", ionizationEnergyProp.doubleValue, textAreaStyle); GUILayout.FlexibleSpace();
+        electronAffinityProp.doubleValue = EditorGUILayout.DoubleField("Electron Affinity", electronAffinityProp.doubleValue, textAreaStyle); GUILayout.FlexibleSpace();
+        meltingPointProp.doubleValue = EditorGUILayout.DoubleField("Melting Point", meltingPointProp.doubleValue, textAreaStyle); GUILayout.FlexibleSpace();
+        boilingPointProp.doubleValue = EditorGUILayout.DoubleField("Boiling Point", boilingPointProp.doubleValue, textAreaStyle); GUILayout.FlexibleSpace();
+        densityProp.doubleValue = EditorGUILayout.DoubleField("Density", densityProp.doubleValue, textAreaStyle); GUILayout.FlexibleSpace();
+        yearDiscoveredProp.stringValue = EditorGUILayout.TextField("Year Discovery", yearDiscoveredProp.stringValue, textAreaStyle); GUILayout.FlexibleSpace();
+    }
+    #endregion
+}
---
+++ Assets/Scripts/ScriptableObject/Element.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ElementData", menuName = "Periodic Element")]
+public class Element : ScriptableObject
+{
+    public enum ElementType { Nonmetal, NobleGas, AlkaliMetal, AlkalineEarth, Metalloid,
+        PostTransMetal, Halogen, TransMetal, Lanthanide, Actinide}
+
+
+    [SerializeField] private int _elementNum;
+    public int ElementNum { get { return _elementNum; } }
+
+    [SerializeField] private string _elementName;
+    public string ElementName { get { return _elementName; } }
+
+    [SerializeField] ElementType _elementType = ElementType.Nonmetal;
+    public ElementType GetElementType { get { return _elementType; } }
+
+    [SerializeField] private double _atomicMass;
+    public double AtomicMass { get { return _atomicMass; } }
+
+    [SerializeField] private string _electronConfig;
+    public string ElectronConfig { get { return _electronConfig; } }
+
+    [SerializeField] private string _oxydationState;
+    public string OxydationState { get { return _oxydationState; } }
+
+    [SerializeField] private double _electronegativity;
+    public double Electronegativiy { get { return _electronegativity; } }
+
+    [SerializeField] private double _atomicRadius;
+    public double AtomicRadius { get { return _atomicRadius; } }
+
+    [SerializeField] private double _ionizationEnergy;
+    public double IonizationEnergy { get { return _ionizationEnergy; } }
+
+    [SerializeField] private double _electronAffinity;
+    public double ElectronAffinity { get { return _electronAffinity; } }
+
+    [SerializeField] private double _meltingPoint;
+    public double MeltingPoint { get { return _meltingPoint; } }
+
+    [SerializeField] private double _boilingPoint;
+    public double BoilingPoint { get { return _boilingPoint; } }
+
+    [SerializeField] private double _density;
+    public double Density { get { return _density; } }
+
+    [SerializeField] private string _yearDiscovered;
+    public double YearDiscovered { get { return YearDiscovered; } }
+}
